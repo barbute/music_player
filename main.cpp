5d34b5ef990eaf79dbbfd12cf23465a19a01b9fd
barbute <115746972+barbute@users.noreply.github.com>
2026-04-22 22:38:01 -0500

    Minor cleanup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,3 @@
-// C++ code
-//
-
 const int BUTTON_PLAY = 13;
 const int BUTTON_PREV = 12;
 const int BUTTON_NEXT = 11;
@@ -21,6 +18,7 @@ struct ColorRGB{
 ColorRGB COLOR_GREEN = { 100, 191, 75 };
 ColorRGB COLOR_ORANGE = { 242, 131, 34 };
 ColorRGB COLOR_RED = { 242, 61, 61 };
+ColorRGB COLOR_OFF = { 0, 0, 0 };
 
 void setup() {
   Serial.begin(9600);
@@ -37,12 +35,6 @@ void setup() {
   pinMode(LED_STATUS_GRE, OUTPUT);
 }
 
-void setStatusColor(int red, int green, int blue) {
-  analogWrite(LED_STATUS_RED, red);
-  analogWrite(LED_STATUS_GRE, green);
-  analogWrite(LED_STATUS_BLU, blue);
-}
-
 void setStatusColor(struct ColorRGB color) {
   analogWrite(LED_STATUS_RED, color.red);
   analogWrite(LED_STATUS_GRE, color.green);
@@ -53,9 +45,11 @@ void loop() {
   Serial.println(digitalRead(BUTTON_PLAY));
   
   if (digitalRead(BUTTON_PLAY)) {
-    setStatusColor(COLOR_ORGANGE);
+    setStatusColor(COLOR_RED);
+    tone(SPEAKER, 250);
   } else {
-    setStatusColor(0, 0, 0);
+    setStatusColor(COLOR_OFF);
+    noTone(SPEAKER);
   }
   
   delay(10);
